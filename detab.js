f67fabebfdd4a892ad058e5935e362021f0cb180
Peter Seibel <seibelp@dnc.org>
2018-01-20 18:38:48 -0800

    Add event listener to kill duplicates as soon as they pop up.

--- detab.js
+++ detab.js
@@ -2,15 +2,38 @@
  * Close duplicate tabs.
  */
 
+function normalizeUrl(url) {
+  return url.split(/[#]/)[0];
+}
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+  if (changeInfo.status == 'complete') {
+    let tabUrl = normalizeUrl(tab.url);
+
+    chrome.tabs.query({}, (tabs) => {
+      for (let i = 0; i < tabs.length; i++) {
+        let otherUrl = normalizeUrl(tabs[i].url);
+        if (otherUrl == tabUrl && tabs[i].id != tabId) {
+          chrome.tabs.update(tabs[i].id, { highlighted: true })
+          chrome.tabs.remove(tabId);
+        }
+      }
+    });
+  }
+});
+
+
 chrome.browserAction.onClicked.addListener(()  => {
 
   chrome.tabs.query({}, (tabs) => {
 
-    var counts = {};
-    var closed = 0;
+    let counts = {};
+    let closed = 0;
 
-    for (var i = 0; i < tabs.length; i++) {
-      var url = tabs[i].url.split(/[?#]/)[0]
+    for (let i = 0; i < tabs.length; i++) {
+      let url = normalizeUrl(tabs[i].url);
       if (url in counts) {
         chrome.tabs.remove(tabs[i].id);
         closed++;
@@ -18,7 +41,7 @@ chrome.browserAction.onClicked.addListener(()  => {
         counts[url] = 1;
       }
     }
-    var p = closed == 1 ? '' : 's';
+    let p = closed == 1 ? '' : 's';
     alert('Closed ' + closed + ' duplicate tab' + p + '.');
   });
 });
