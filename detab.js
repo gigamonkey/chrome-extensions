c565d230145900f02825e62f6c09969f401c3e7c
Peter Seibel <seibelp@dnc.org>
2018-01-23 17:31:06 -0800

    Make detabing more aggressive for icon click.

--- detab.js
+++ detab.js
@@ -2,19 +2,23 @@
  * Close duplicate tabs.
  */
 
-function normalizeUrl(url) {
-  return url.split(/[#]/)[0];
+function normalizeUrl(url, aggressive) {
+  if (aggressive) {
+    return url.split(/[?#]/)[0];
+  } else {
+    return url.split(/[#]/)[0];
+  }
 }
 
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
   if (changeInfo.status == 'complete') {
-    let tabUrl = normalizeUrl(tab.url);
+    let tabUrl = normalizeUrl(tab.url, false);
 
     chrome.tabs.query({}, (tabs) => {
       for (let i = 0; i < tabs.length; i++) {
-        let otherUrl = normalizeUrl(tabs[i].url);
+        let otherUrl = normalizeUrl(tabs[i].url, false);
         if (otherUrl == tabUrl && tabs[i].id != tabId) {
           chrome.tabs.update(tabs[i].id, { highlighted: true })
           chrome.tabs.remove(tabId);
@@ -33,7 +37,7 @@ chrome.browserAction.onClicked.addListener(()  => {
     let closed = 0;
 
     for (let i = 0; i < tabs.length; i++) {
-      let url = normalizeUrl(tabs[i].url);
+      let url = normalizeUrl(tabs[i].url, true);
       if (url in counts) {
         chrome.tabs.remove(tabs[i].id);
         closed++;
